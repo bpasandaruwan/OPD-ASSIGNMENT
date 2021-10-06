71dc35288f6db2db9b60595f8fca481dd338d521
Lakma Manduli <lakmamanduli.ja@gmail.com>
2021-10-07 01:17:38 +0530

    create complain form(design interface)

---
+++ Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using system Data.SqlClient;
+
+namespace complain
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clearbutton1_Click(object sender, EventArgs e)
+        {
+            txtid.Text = "";
+            txtmobile.Text = "";
+            cmb_ctype.Text = "";
+            cmb_cby.Text = "";
+            txtdescription.Text = "";
+            cmb_adby.Text = "";
+            dateTimePicker1.Text = "";
+            dateTimePicker1.Text = "";
+        }
+
+        private void openbutton1_Click(object sender, EventArgs e)
+        {
+            if (result == DialogResult.OK)
+            {
+                //string file = OpenFileDialog.filename;
+
+                //string[] f = file.Split('\\');
+                //string[] f = f[(f.Length) - 1];
+                //string dest = @"G:\opdsys\c#\uploadimg";
+                //file.CopyTo(file, DesktopBounds, true);
+
+                //string q = "insert into [data_file]values('" + fn + "','" + dest + "')";
+                //ProcessCmdKey().ExecuteNonQuery();
+                //MessageBox.Show("success");
+            }
+        }
+
+        /private void open_FileOk(object sender, CancelEventArgs e)
+        {
+            DialogResult result = OpenFileDialog1.showDialog();
+            if (result == DialogResult.OK)
+            {
+                string file = OpenFileDialog1.filename;
+
+                string[] f = file.Split('\\');
+                string[] f = f[(f.Length) - 1];
+                string dest = @"G:\opdsys\c#\uploadimg";
+                file.CopyTo(file, DesktopBounds, true);
+
+                string q = "insert into [data_file]values('" + fn + "','" + dest + "')";
+                ProcessCmdKey().ExecuteNonQuery();
+                MessageBox.Show("success");
+            }
+        }
+
+        private void submitbutton2_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new sqlConnection("Data Source=LAKMA\SQLEXPRESS;Initial Catalog = itest;Intergrated Security=True");
+            con.open();
+            sqlCommand cmd = new sqlCommand("insert into complain"id
+                )values (txtid.Text,txtmobile.Text,cmb_ctype.Items,cmb_cby.Items)values('"+')
+                int i = cmd.ExecuteNonquery();
+            if (i != 0)
+            {
+                MessageBox.Show("Saved");
+            }
+            else
+            {
+                MessageBox.Show("Error");
+            }
+            con.close();
+        }*/
+    }
+}
---
+++ Form1.designer.cs
@@ -0,0 +1,304 @@
+﻿
+namespace complain
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.cmb_ctype = new System.Windows.Forms.ComboBox();
+            this.cmb_cby = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.txtid = new System.Windows.Forms.TextBox();
+            this.txtmobile = new System.Windows.Forms.TextBox();
+            this.txtdescription = new System.Windows.Forms.RichTextBox();
+            this.cmb_adby = new System.Windows.Forms.ComboBox();
+            this.clearbutton1 = new System.Windows.Forms.Button();
+            this.submitbutton2 = new System.Windows.Forms.Button();
+            this.closebutton3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NIC Number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mobile Number";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Complain Type";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Complain By";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(40, 196);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Description";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(40, 285);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(102, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Attached Document";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(40, 315);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Added By";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(40, 359);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(30, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Date";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(40, 396);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(97, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Action Taken Date";
+            // 
+            // cmb_ctype
+            // 
+            this.cmb_ctype.FormattingEnabled = true;
+            this.cmb_ctype.Items.AddRange(new object[] {
+            "one",
+            "two"});
+            this.cmb_ctype.Location = new System.Drawing.Point(172, 113);
+            this.cmb_ctype.Name = "cmb_ctype";
+            this.cmb_ctype.Size = new System.Drawing.Size(121, 21);
+            this.cmb_ctype.TabIndex = 9;
+            // 
+            // cmb_cby
+            // 
+            this.cmb_cby.FormattingEnabled = true;
+            this.cmb_cby.Items.AddRange(new object[] {
+            "one",
+            "two"});
+            this.cmb_cby.Location = new System.Drawing.Point(172, 153);
+            this.cmb_cby.Name = "cmb_cby";
+            this.cmb_cby.Size = new System.Drawing.Size(121, 21);
+            this.cmb_cby.TabIndex = 10;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(172, 353);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 11;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(172, 390);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 12;
+            this.dateTimePicker2.ValueChanged += new System.EventHandler(this.dateTimePicker2_ValueChanged);
+            // 
+            // txtid
+            // 
+            this.txtid.Location = new System.Drawing.Point(172, 32);
+            this.txtid.Name = "txtid";
+            this.txtid.Size = new System.Drawing.Size(100, 20);
+            this.txtid.TabIndex = 13;
+            // 
+            // txtmobile
+            // 
+            this.txtmobile.Location = new System.Drawing.Point(172, 74);
+            this.txtmobile.Name = "txtmobile";
+            this.txtmobile.Size = new System.Drawing.Size(100, 20);
+            this.txtmobile.TabIndex = 14;
+            // 
+            // txtdescription
+            // 
+            this.txtdescription.Location = new System.Drawing.Point(172, 193);
+            this.txtdescription.Name = "txtdescription";
+            this.txtdescription.Size = new System.Drawing.Size(268, 65);
+            this.txtdescription.TabIndex = 15;
+            this.txtdescription.Text = "";
+            // 
+            // cmb_adby
+            // 
+            this.cmb_adby.FormattingEnabled = true;
+            this.cmb_adby.Items.AddRange(new object[] {
+            "one",
+            "two"});
+            this.cmb_adby.Location = new System.Drawing.Point(172, 312);
+            this.cmb_adby.Name = "cmb_adby";
+            this.cmb_adby.Size = new System.Drawing.Size(121, 21);
+            this.cmb_adby.TabIndex = 16;
+            // 
+            // clearbutton1
+            // 
+            this.clearbutton1.Location = new System.Drawing.Point(25, 426);
+            this.clearbutton1.Name = "clearbutton1";
+            this.clearbutton1.Size = new System.Drawing.Size(75, 23);
+            this.clearbutton1.TabIndex = 17;
+            this.clearbutton1.Text = "Clear";
+            this.clearbutton1.UseVisualStyleBackColor = true;
+            this.clearbutton1.Click += new System.EventHandler(this.clearbutton1_Click);
+            // 
+            // submitbutton2
+            // 
+            this.submitbutton2.Location = new System.Drawing.Point(218, 426);
+            this.submitbutton2.Name = "submitbutton2";
+            this.submitbutton2.Size = new System.Drawing.Size(75, 23);
+            this.submitbutton2.TabIndex = 18;
+            this.submitbutton2.Text = "Submit";
+            this.submitbutton2.UseVisualStyleBackColor = true;
+            this.submitbutton2.Click += new System.EventHandler(this.submitbutton2_Click);
+            // 
+            // closebutton3
+            // 
+            this.closebutton3.Location = new System.Drawing.Point(416, 426);
+            this.closebutton3.Name = "closebutton3";
+            this.closebutton3.Size = new System.Drawing.Size(75, 23);
+            this.closebutton3.TabIndex = 19;
+            this.closebutton3.Text = "Close";
+            this.closebutton3.UseVisualStyleBackColor = true;
+            this.closebutton3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(172, 264);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 37);
+            this.pictureBox1.TabIndex = 20;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 461);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.closebutton3);
+            this.Controls.Add(this.submitbutton2);
+            this.Controls.Add(this.clearbutton1);
+            this.Controls.Add(this.cmb_adby);
+            this.Controls.Add(this.txtdescription);
+            this.Controls.Add(this.txtmobile);
+            this.Controls.Add(this.txtid);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.cmb_cby);
+            this.Controls.Add(this.cmb_ctype);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "COMPLAIN FORM";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ComboBox cmb_ctype;
+        private System.Windows.Forms.ComboBox cmb_cby;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.TextBox txtid;
+        private System.Windows.Forms.TextBox txtmobile;
+        private System.Windows.Forms.RichTextBox txtdescription;
+        private System.Windows.Forms.ComboBox cmb_adby;
+        private System.Windows.Forms.Button clearbutton1;
+        private System.Windows.Forms.Button submitbutton2;
+        private System.Windows.Forms.Button closebutton3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
